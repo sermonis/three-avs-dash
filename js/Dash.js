3441491395635fb8f782d1945a2661899fa0efe3
woodell <dev@sermonis.ru>
2021-07-05 08:21:49 +0300

    dash refactored

--- js/Dash.js
+++ js/Dash.js
@@ -1,6 +1,8 @@
 import Simulator from './Simulator.js';
 import StaticObstacle from './autonomy/StaticObstacle.js';
 
-document.addEventListener('DOMContentLoaded', e => {
-  window.simulator = new Simulator(document.getElementById('container'));
-});
+document.addEventListener( 'DOMContentLoaded', e => {
+
+	window.simulator = new Simulator( document.getElementById( 'container' ) );
+
+} );
