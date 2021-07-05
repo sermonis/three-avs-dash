e4e81da64af4d3a45bc3d75fe5a9dcb7d745ad34
woodell <dev@sermonis.ru>
2021-07-05 08:23:05 +0300

    helpers refactored

--- js/Helpers.js
+++ js/Helpers.js
@@ -1,5 +1,16 @@
-function formatDate(date) {
-  return date && date.toLocaleDateString(undefined, {month: 'short', day: 'numeric', year: 'numeric', hour: 'numeric', minute: 'numeric', hour12: true});
+function formatDate ( date ) {
+
+	return date && date.toLocaleDateString( undefined, {
+
+		month: 'short',
+		day: 'numeric',
+		year: 'numeric',
+		hour: 'numeric',
+		minute: 'numeric',
+		hour12: true,
+
+	} );
+
 }
 
 export { formatDate };
